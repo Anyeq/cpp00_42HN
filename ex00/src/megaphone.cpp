4db416144456b333d4f72347144e04aee4f0da5f
asando <alexando17@yahoo.com>
2026-05-10 16:23:38 +0200

    WIP: Makefile

--- ex00/src/megaphone.cpp
+++ ex00/src/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 14:35:40 by asando            #+#    #+#             */
-/*   Updated: 2026/05/10 15:42:58 by asando           ###   ########.fr       */
+/*   Updated: 2026/05/10 15:46:10 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,6 @@ int	main(int argc, char **argv)
 			for (int j = 0; argv[i][j] != '\0'; j++)
 				argv[i][j] = (char)toupper(argv[i][j]);
 			std::cout << argv[i];
-			if (i < argc - 1)
-				std::cout << " ";
 		}
 		std::cout << std::endl;
 	}
