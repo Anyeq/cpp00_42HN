25f3abd084bf3973373c8814ff8476e22233bd9e
asando <alexando17@yahoo.com>
2026-05-14 15:36:04 +0200

    WIP on ex01 creating class

---
+++ ex01/include/Contact.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/14 15:23:24 by asando            #+#    #+#             */
+/*   Updated: 2026/05/14 15:35:32 by asando           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <string>
+
+class	Contact{
+	private:
+		std::string	_firstName;
+		std::string	_lastName;
+		std::string _nickName;
+		std::string	_phoneNumber;
+		std::string	_darkestSecret;
+	public:
+
+};
+
+#endif
---
+++ ex01/include/PhoneBook.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/14 15:12:29 by asando            #+#    #+#             */
+/*   Updated: 2026/05/14 15:31:04 by asando           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include "Contact.hpp"
+class	PhoneBook {
+	private:
+		Contact	contact[8];
+};
+
+#endif
