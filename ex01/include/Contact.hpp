b6a1c8418385b2f8c5e124dfdadd9248e9fa1a16
asando <alexando17@yahoo.com>
2026-05-15 13:07:51 +0200

    ADD: Contach.cpp

--- ex01/include/Contact.hpp
+++ ex01/include/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/14 15:23:24 by asando            #+#    #+#             */
-/*   Updated: 2026/05/14 15:35:32 by asando           ###   ########.fr       */
+/*   Updated: 2026/05/14 15:53:01 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,10 @@ class	Contact{
 		std::string	_phoneNumber;
 		std::string	_darkestSecret;
 	public:
-
+		void	setFirstName(const std::string &str);
+		void	setLastName(const std::string &str);
+		void	setNickName(const std::string &str);
+		void	setPhoneNumber(const std::string &str);
+		void	setDarkestSecret(const std::string &str);
 };
-
 #endif
---
+++ ex01/src/Contact.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/14 15:49:34 by asando            #+#    #+#             */
+/*   Updated: 2026/05/15 13:07:06 by asando           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "Contact.hpp"
+
+void	Contact::setFirstName(const std::string &str) 
+{ 
+	this->_firstName = str;
+	return ;
+}
+
+void	Contact::setLastName(const std::string &str)
+{
+	this->_lastName = str;
+	return ;
+}
+
+void	Contact::setNickName(const std::string &str)
+{
+	this->_nickName = str;
+	return ;
+}
+
+void	Contact::setPhoneNumber(const std::string &str)
+{
+	this->_phoneNumber = str;
+	return ;
+}
+
+void	Contact:: darkestSecret(const std::string &str)
+{
+	this->_darkestSecret= str;
+	return ;
+}
