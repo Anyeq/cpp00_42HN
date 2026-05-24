8a92ad3d162e273d2296275d859da41fb0833229
asando <alexando17@yahoo.com>
2026-05-24 23:18:30 +0200

    WIP: ex02

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,11 +6,13 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/18 21:22:43 by asando            #+#    #+#             */
-/*   Updated: 2026/05/24 21:14:46 by asando           ###   ########.fr       */
+/*   Updated: 2026/05/24 23:15:25 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <ctime>
+#include <iomanip>
 
 int	Account::_nbAccounts = 0;
 int	Account::_totalAmount = 0;
@@ -26,13 +28,15 @@ Account::Account(int initial_deposit)
 	_nbAccounts++;
 	_totalAmount += initial_deposit;
 	_displayTimestamp();
+
 	std::cout << "index:" << _accountIndex
 		<< ";amount:" << _amount
 		<< ";created:" << std::endl;
 }
 
 Account::~Account() {
-	_displatTimestamp();
+	_displayTimestamp();
+
 	std::cout << "index:" << _accountIndex
 		<< ";amount:" << _amount
 		<< ";created:" << std::endl;
@@ -41,6 +45,7 @@ Account::~Account() {
 void	Account::_displayTimestamp() {
 	std::time_t now = std::time(NULL);
 	std::tm *t = std::localtime(&now);
+
 	std::cout << "[" << (t->tm_year + 1900)
 		<< std::setw(2) << std::setfill('0') <<t->tm_mon + 1
 		<< std::setw(2) << t->tm_day << "_"
@@ -48,4 +53,41 @@ void	Account::_displayTimestamp() {
 		<< std::setw(2) << t->tm_min
 		<< std::setw(2) << t->tm_sec
 		<< "] ";
+}
+
+void	Account::displayAccountsInfos() {
+	_displayTimestamp();
+
+	std::cout << "accounts:" <<_nbAccounts
+		<< ";total:" << _totalAmount
+		<< ";deposit:" << _totalNbDeposit
+		<< ";withdrawals:" << _totalNbWithdrawals
+		<< std::endl;
+}
+
+void	Account::displayStatus(){
+	_displayTimestamp();
+
+	std::cout << "index:" << _accountIndex
+		<< ";amount:" << _amount
+		<< ";deposit:" << _nbDeposits
+		<< ";withdrawals" << _totalnbwithdrawals
+		<< std::endl;
+}
+
+void	Account::makeDeposit(int deposit) {
+	_displayTimestamp();
+
+	std::cout << "index:" << _accountIndex
+		<< ";p_amount:" << _amount;
+
+	_amount += deposit;
+	_nbDeposit++;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
+
+	std::cout << ";deposit:" << deposit
+		<< ";amount:" << _nbDeposit
+		<< ";nb_deposits:" << _nbDeposits
+		<< std:endl;
 }
